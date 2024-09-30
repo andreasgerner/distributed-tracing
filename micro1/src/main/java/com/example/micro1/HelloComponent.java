1f16a3aa5487dda4752b7801459253a979a00e35
Andreas Gerner <gerner.andreas12@gmail.com>
2024-09-30 13:58:38 +0200

    ✨ add source for sample micro1 application

---
+++ micro1/src/main/java/com/example/micro1/HelloComponent.java
@@ -0,0 +1,25 @@
+package com.example.micro1;
+
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class HelloComponent {
+
+    @Value("${MICRO2_URL}")
+    private String baseUrl;
+
+    @WithSpan
+    public Mono<String> fetchHelloName() {
+        return WebClient.create(baseUrl)
+                .get()
+                .uri("/name")
+                .retrieve()
+                .bodyToMono(String.class)
+                .map("Hello %s"::formatted);
+    }
+
+}
---
+++ micro1/src/main/java/com/example/micro1/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.micro1;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+
+    private final HelloComponent helloComponent;
+
+    public HelloController(HelloComponent helloComponent) {
+        this.helloComponent = helloComponent;
+    }
+
+    @CrossOrigin
+    @GetMapping("/hello")
+    public Mono<String> getHello() {
+        return helloComponent.fetchHelloName();
+    }
+
+}
