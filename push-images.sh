089680c14b281b589bbf985c3541085f4ce13d4d
Andreas Gerner <gerner.andreas12@gmail.com>
2024-11-26 10:23:38 +0100

    🔨 add script to push all images

---
+++ push-images.sh
@@ -0,0 +1,11 @@
+docker push artifactory:5000/andreasgerner/distributed-tracing/sample-company:latest
+docker push artifactory:5000/andreasgerner/distributed-tracing/sample-payment:latest
+docker push artifactory:5000/andreasgerner/distributed-tracing/sample-web-next:latest
+docker push artifactory:5000/andreasgerner/distributed-tracing/sample-web-angular:local
+docker push artifactory:5000/andreasgerner/distributed-tracing/sample-web-angular:openshift
+
+docker push ghcr.io/andreasgerner/distributed-tracing/sample-company:latest
+docker push ghcr.io/andreasgerner/distributed-tracing/sample-payment:latest
+docker push ghcr.io/andreasgerner/distributed-tracing/sample-web-next:latest
+docker push ghcr.io/andreasgerner/distributed-tracing/sample-web-angular:local
+docker push ghcr.io/andreasgerner/distributed-tracing/sample-web-angular:openshift
